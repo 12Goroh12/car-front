eac23c57c7d26e144d6546c3b74171cb7ade1ff4
Gorohov <d.gorohov@itransition.com>
2022-02-21 17:17:32 +0300

    corrected

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,14 +1,10 @@
-import { FC, ReactNode } from "react";
+import { FC } from "react";
 import { Wrapper } from "./style";
 import { observer } from "mobx-react-lite";
 import Navbar from "../Navbar";
 import carStore from "../../store/carStore";
 
-interface LayoutProps {
-  children: ReactNode;
-}
-
-const Layout: FC<LayoutProps> = ({ children }) => {
+const Layout: FC = ({ children }) => {
   const popupHide = () => {
     carStore.hidePopup();
   };
