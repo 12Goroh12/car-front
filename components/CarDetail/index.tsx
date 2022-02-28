d1d4e41bb674e690b55cc758c86bbcff74d41910
Gorohov <d.gorohov@itransition.com>
2022-02-28 16:40:10 +0300

    added detail content

--- components/CarDetail/index.tsx
+++ components/CarDetail/index.tsx
@@ -1,14 +1,25 @@
 import axios from "axios";
+import { NextRouter, useRouter } from "next/router";
 import { FC, useEffect, useState } from "react";
-import { Container } from "../../styles/carDetail";
+import {
+  Block,
+  Button,
+  ButtonGroup,
+  Container,
+  Heading,
+  SpeedBlock,
+} from "./style";
 import { ICar } from "../../types/cars";
 import { BaseUrl } from "../../types/enums";
+import Image from "next/image";
 
 interface ICarDetailProps {
   carId: string;
 }
 
 const CarDetail: FC<ICarDetailProps> = ({ carId }) => {
+  const { back }: NextRouter = useRouter();
+  const [modal, setModal] = useState(false);
   const [carDetail, setCarDetail] = useState<ICar>();
 
   useEffect(() => {
@@ -23,10 +34,42 @@ const CarDetail: FC<ICarDetailProps> = ({ carId }) => {
     fetchDetailCar();
   }, [carId]);
 
+  const showModal = () => {
+    setModal(!modal);
+  };
+
   return (
     <Container imgUrl={`${BaseUrl.CLOUDINARY_IMAGE}/${carDetail?.file[0]}`}>
-      {/* there will be content */}
-      <h1>{carDetail?.name}</h1>
+      <Heading>
+        <h1>{carDetail?.name}</h1>
+        <p>{carDetail?.description}</p>
+        <SpeedBlock>
+          <Block>
+            <Image src="/power.png" alt="speed" width={20} height={30} />
+            <p>{carDetail?.reserve} km</p>
+            <span>power reserve</span>
+          </Block>
+          <Block>
+            <Image src="/speed.png" alt="speed" width={30} height={20} />
+            <p>{carDetail?.speed} km</p>
+            <span>0-100 кm/h</span>
+          </Block>
+          {carDetail?.used === true && (
+            <Block>
+              <span>mileage</span>
+              <p>{carDetail?.mileage} km</p>
+            </Block>
+          )}
+        </SpeedBlock>
+        <Block>
+          <span>Price: {carDetail?.price} $</span>
+        </Block>
+      </Heading>
+      {modal && <p>Form test drive</p>}
+      <ButtonGroup>
+        <Button onClick={showModal}>Test Drive</Button>
+        <Button onClick={back}>Back</Button>
+      </ButtonGroup>
     </Container>
   );
 };
