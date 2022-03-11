a78a8ea09a69f38f5134699988a375cf55295504
Gorohov <d.gorohov@itransition.com>
2022-03-11 13:39:07 +0300

    redirect

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,16 +1,23 @@
+import { NextRouter, useRouter } from "next/router";
 import { FC, useEffect, useState } from "react";
+import { Roles, WebsiteUrls } from "../../types/enums";
 import SignIn from "../auth/SignIn";
 import SignOut from "../auth/SignOut";
+import Redirect from "../Redirect";
 
 const Navrabr: FC = () => {
+  const { pathname }: NextRouter = useRouter();
   const [hasMounted, setHasMounted] = useState(false);
   const [showNavbar, setNavbarShow] = useState(false);
+
   let user;
   let userSocial;
+
   if (typeof window !== "undefined") {
     user = JSON.parse(localStorage.getItem("user") || "null");
     userSocial = JSON.parse(localStorage.getItem("social") || "null");
   }
+  const isUser = user?.roles[0] === Roles.USER;
 
   useEffect(() => {
     setHasMounted(true);
@@ -29,6 +36,12 @@ const Navrabr: FC = () => {
 
   if (!hasMounted) {
     return null;
+  }
+
+  if (pathname === WebsiteUrls.CREATE_CAR) {
+    if (isUser || !user || !userSocial) {
+      return <Redirect to={WebsiteUrls.HOME} />;
+    }
   }
 
   return user || userSocial ? (
