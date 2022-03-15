7b07ffb72c6bbe8294c0706d547a29e1c20475dd
Gorohov <d.gorohov@itransition.com>
2022-03-15 14:43:30 +0300

    added test for email and password

--- __test__/LoginComponent.test.tsx
+++ __test__/LoginComponent.test.tsx
@@ -1,5 +1,7 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { RouterContext } from "next/dist/shared/lib/router-context";
+import { createMockRouter } from ".";
 import LoginComponent from "../components/LoginComponent";
 import { WebsiteUrls } from "../types/enums";
 
@@ -85,7 +87,7 @@ describe("render the component", () => {
     expect(screen.getByDisplayValue(/admin/i)).toBeInTheDocument();
   });
 
-  it("should show validation on blur", async () => {
+  it("should show validation on blur field name", async () => {
     const submit = jest.fn();
     render(
       <LoginComponent
@@ -109,5 +111,169 @@ describe("render the component", () => {
         "name is a required field"
       );
     });
+  });
+
+  it("field email", () => {
+    const submit = jest.fn();
+    render(
+      <LoginComponent
+        heading="Registration"
+        link="Log in with account social"
+        href={WebsiteUrls.SOCIAL}
+        exist={false}
+        submitHandler={submit}
+        error="This Email already exists"
+        account="have an account?"
+      />
+    );
+
+    const email = screen.getByLabelText(/email/i);
+
+    expect(email).toBeInTheDocument();
+    expect(email).not.toBeRequired();
+    expect(email).not.toHaveFocus();
+    email.focus();
+    expect(email).toHaveFocus();
+
+    userEvent.type(email, "test@test.com");
+
+    expect(screen.getByDisplayValue(/test@test.com/i)).toBeInTheDocument();
+  });
+
+  it("should show validation on blur field email", async () => {
+    const submit = jest.fn();
+    render(
+      <LoginComponent
+        heading="Registration"
+        link="Log in with account social"
+        href={WebsiteUrls.SOCIAL}
+        exist={false}
+        submitHandler={submit}
+        error="This Email already exists"
+        account="have an account?"
+      />
+    );
+
+    const email = screen.getByLabelText(/email/i);
+
+    fireEvent.blur(email);
+
+    await waitFor(() => {
+      expect(screen.getByTestId("error")).not.toBe(null);
+      expect(screen.getByTestId("error")).toHaveTextContent(
+        "email is a required field"
+      );
+    });
+  });
+
+  it("should show validation on blur field password", async () => {
+    const submit = jest.fn();
+    render(
+      <LoginComponent
+        heading="Registration"
+        link="Log in with account social"
+        href={WebsiteUrls.SOCIAL}
+        exist={false}
+        submitHandler={submit}
+        error="This Email already exists"
+        account="have an account?"
+      />
+    );
+
+    const email = screen.getByLabelText(/password/i);
+
+    fireEvent.blur(email);
+
+    await waitFor(() => {
+      expect(screen.getByTestId("errorPass")).not.toBe(null);
+      expect(screen.getByTestId("errorPass")).toHaveTextContent(
+        "password is a required field"
+      );
+    });
+  });
+
+  it("field password", () => {
+    const submit = jest.fn();
+    render(
+      <LoginComponent
+        heading="Registration"
+        link="Log in with account social"
+        href={WebsiteUrls.SOCIAL}
+        exist={false}
+        submitHandler={submit}
+        error="This Email already exists"
+        account="have an account?"
+      />
+    );
+
+    const password = screen.getByLabelText(/password/i);
+
+    expect(password).toBeInTheDocument();
+    expect(password).not.toBeRequired();
+    expect(password).not.toHaveFocus();
+    password.focus();
+    expect(password).toHaveFocus();
+
+    userEvent.type(password, "123456");
+
+    expect(screen.getByDisplayValue(/123456/i)).toBeInTheDocument();
+  });
+
+  it("button submit", async () => {
+    const submit = jest.fn();
+    render(
+      <LoginComponent
+        heading="Registration"
+        link="Log in with account social"
+        href={WebsiteUrls.SOCIAL}
+        exist={false}
+        submitHandler={submit}
+        error="This Email already exists"
+        account="have an account?"
+      />
+    );
+
+    const button = screen.getByRole("button", { name: /submit/i });
+    const email = screen.getByLabelText(/password/i);
+
+    fireEvent.blur(email);
+
+    expect(button).toBeInTheDocument();
+    expect(button).not.toBeDisabled();
+
+    await waitFor(() => {
+      expect(screen.getByTestId("errorPass")).not.toBe(null);
+      expect(screen.getByTestId("errorPass")).toHaveTextContent(
+        "password is a required field"
+      );
+      expect(button).toBeDisabled();
+    });
+  });
+
+  it("Sign in with Social work", async () => {
+    const router = createMockRouter({
+      query: { pathname: WebsiteUrls.REGISTRATION },
+    });
+    const submit = jest.fn();
+    render(
+      <RouterContext.Provider value={router}>
+        <LoginComponent
+          heading="Registration"
+          link="Log in with account social"
+          href={WebsiteUrls.SOCIAL}
+          exist={false}
+          submitHandler={submit}
+          error="This Email already exists"
+          account="have an account?"
+        />
+      </RouterContext.Provider>
+    );
+
+    const button = screen.getByText(/Log in with account social/i);
+    const haveAnAccount = screen.getByText(/have an account?/i);
+
+    expect(haveAnAccount).toBeInTheDocument();
+    expect(haveAnAccount).toHaveAttribute("href", WebsiteUrls.LOGIN);
+    expect(button).toHaveAttribute("href", WebsiteUrls.SOCIAL);
   });
 });
